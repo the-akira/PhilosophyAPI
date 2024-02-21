07bc581f0509d21e13e66fd761e634529f5b96ad
Gabriel Felippe <gabrielfelippe@outlook.com>
2024-02-21 00:01:19 -0300

    Links Sociais Atualizados

--- philosophyapi/settings.py
+++ philosophyapi/settings.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,5 +126,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "philosophyapi/static"),
 ]
-
-django_heroku.settings(locals())
